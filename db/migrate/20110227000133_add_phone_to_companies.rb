a6fe3d5e6d5ff39384b2cbe2528640f7707a0753
Rafa García <contacto@rafagarcia.net>
2011-02-27 01:12:05 +0100

    Add phone to company

---
+++ db/migrate/20110227000133_add_phone_to_companies.rb
@@ -0,0 +1,9 @@
+class AddPhoneToCompanies < ActiveRecord::Migration
+  def self.up
+    add_column :companies, :phone, :string
+  end
+
+  def self.down
+    remove_column :companies, :phone
+  end
+end
